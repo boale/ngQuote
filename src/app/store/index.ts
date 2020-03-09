f9cc6955c83972bdc4a88090007a70e970599d06
Oleksandr Bondarenko <Oleksandr_Bondarenko2@epam.com>
2020-03-09 23:22:17 +0200

    feat: implement quote generation workflow with ngrx
    remove unnecessary imports

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,12 +1,10 @@
 import * as fromRouter from '@ngrx/router-store';
 import {
-  Action,
   ActionReducer,
   ActionReducerMap,
   MetaReducer,
 } from '@ngrx/store';
 
-import { InjectionToken } from '@angular/core';
 import { environment } from '../../environments/environment';
 
 // tslint:disable-next-line:no-empty-interface
