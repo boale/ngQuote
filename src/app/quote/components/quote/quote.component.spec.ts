5070c45989da5d1a9c34c7871c812531bb3ab0e2
Oleksandr Bondarenko <Oleksandr_Bondarenko2@epam.com>
2020-03-16 20:25:11 +0200

    test: add basic quote generator
    add test coverage up to required level

--- src/app/quote/components/quote/quote.component.spec.ts
+++ src/app/quote/components/quote/quote.component.spec.ts
@@ -1,33 +1,71 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { Quote } from '../../models';
 import { QuoteComponent } from './quote.component';
 
-describe('QuoteComponent', () => {
-  let component: QuoteComponent;
-  let fixture: ComponentFixture<QuoteComponent>;
+const mockQuote: Quote = {
+  text: 'test quote text',
+  author: 'me',
+};
 
-  const mockQuote: Quote = {
-    text: 'test quote text',
-    author: 'me',
-  };
+@Component({
+  template: `<app-quote [quote]="quote"></app-quote>`,
+})
+export class TestHostQuoteComponent {
+  quote: Quote = mockQuote;
+}
+
+describe('QuoteComponent', () => {
+  let component: TestHostQuoteComponent;
+  let quoteComponentElem;
+  let fixture: ComponentFixture<TestHostQuoteComponent>;
+
+  const getQuoteDebugElement = () => fixture.debugElement.query(By.css('.quote'));
+  const getBlockquoteTextDebugElement = () => fixture.debugElement.query(By.css('blockquote p'));
+  const getQuoteAuthorDebugElement = () => fixture.debugElement.query(By.css('cite'));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuoteComponent ],
+      declarations: [ QuoteComponent, TestHostQuoteComponent ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(QuoteComponent);
+    fixture = TestBed.createComponent(TestHostQuoteComponent);
     component = fixture.componentInstance;
-    component.quote = mockQuote;
-
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not render quote component if quote has not been provided', () => {
+    component.quote = null;
+    fixture.detectChanges();
+
+    quoteComponentElem = getQuoteDebugElement();
+
+    expect(quoteComponentElem).toBeFalsy();
+  });
+
+  it('should render quote component', () => {
+    quoteComponentElem = getQuoteDebugElement();
+
+    expect(quoteComponentElem).toBeTruthy();
+  });
+
+  it('should contain quote text', () => {
+    const quoteText = getBlockquoteTextDebugElement().nativeElement.innerText;
+    expect(quoteText).toContain(mockQuote.text);
+  });
+
+  it('should contain quote authoe', () => {
+    const quoteAuthor = getQuoteAuthorDebugElement().nativeElement.innerText;
+    expect(quoteAuthor).toContain(mockQuote.author);
+  });
+
 });
--- src/app/quote/services/quote.service.spec.ts
+++ src/app/quote/services/quote.service.spec.ts
@@ -1,7 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 
-import { QuoteService } from './quote.service';
+import { Observable } from 'rxjs';
+
+import { Quote } from '../models';
+import { QuoteService, QUOTESY } from './quote.service';
 import { mockQuotesyProvider } from './quote.service.mock';
+
+const mockQuote: Quote = {
+  text: 'quote text',
+  author: 'author',
+};
 
 describe('QuoteService', () => {
   let service: QuoteService;
@@ -16,4 +24,19 @@ describe('QuoteService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return an Observable of quote object', () => {
+    const quote = service.getRandom();
+    expect(quote).toEqual(jasmine.any(Observable));
+  });
+
+  it('should execute quotesy to get random quote', async(inject([QUOTESY], (quotesy) => {
+    const spy = spyOn(quotesy, 'random').and.returnValue(mockQuote);
+
+    service.getRandom().subscribe((quote) => {
+      expect(spy).toHaveBeenCalled();
+      expect(quote).toEqual(mockQuote);
+    });
+  })));
+
 });
