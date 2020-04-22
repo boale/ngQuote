8f1c950d5b86cf4c3cf2c5bbe4ddff10b847ec56
ainura_aliieva <Ainura_Aliieva@epam.com>
2020-04-22 15:53:45 +0300

    feat: update api url

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   apiUrls: {
-    quote: 'http://quote-publi-jv87ocwvh0p8-1628581181.us-east-1.elb.amazonaws.com/api',
+    quote: 'http://ng-qu-publi-10odnblm1hccm-1462714472.us-east-1.elb.amazonaws.com/api',
   },
 };
